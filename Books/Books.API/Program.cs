3ea550152bd885093b0be68833f83d814af328e5
elieljohn <ejmalabar88@gmail.com>
2024-12-11 16:46:56 +0000

    Implementing the Repository Contract

--- Books/Books.API/Program.cs
+++ Books/Books.API/Program.cs
@@ -1,4 +1,5 @@
 using Books.API.DbContexts;
+using Books.API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,8 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<BooksContext>(options =>
     options.UseSqlite(
         builder.Configuration["ConnectionStrings:BooksDBConnectionString"]));
+
+builder.Services.AddScoped<IBooksRepository, BooksRepository>();
 
 var app = builder.Build();
 
---
+++ Books/Books.API/Services/BooksRepository.cs
@@ -0,0 +1,29 @@
+﻿using Books.API.DbContexts;
+using Books.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Books.API.Services;
+
+public class BooksRepository : IBooksRepository
+{
+    private readonly BooksContext _context;
+
+    public BooksRepository(BooksContext context)
+    {
+        _context = context ??
+            throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task<Book?> GetBookAsync(Guid id)
+    {
+        return await _context.Books
+            .FirstOrDefaultAsync(b => b.Id == id);
+    }
+
+    public async Task<IEnumerable<Book>> GetBooksAsync()
+    {
+        return await _context.Books
+            .Include(b => b.Author)
+            .ToListAsync();
+    }
+}
