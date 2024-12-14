6a7d4178e74342ac2f939cbaa27935ae9a4d558d
elieljohn <ejmalabar88@gmail.com>
2024-12-14 15:59:22 +0000

    Adding and Configuring AutoMapper

--- Books/Books.API/Program.cs
+++ Books/Books.API/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddDbContext<BooksContext>(options =>
 
 builder.Services.AddScoped<IBooksRepository, BooksRepository>();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
